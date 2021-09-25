846d50b31a67ecea79eea6c6cb8ca87a117dac36
Paolo <paolo.hitoshi@gmail.com>
2021-09-25 19:32:05 -0300

    Menu Principal
    Botões de jogar, sair e opções feitos.
    Opções de fullscreen, volume e resolução terminados.

---
+++ MegaFoguinho/Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public string levelName;
+
+    public void LoadGameScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
