f3376f7b261eae074797e6c7eb0ebd72c2a5e2b0
Luana <ltdc01@hotmail.com>
2021-09-25 22:40:49 -0300

    Auto stash before merge of "main" and "origin/main"

---
+++ MegaFoguinho/Assets/Scripts/Foguinho/AnimationCopy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCopy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetLimb;
+    private ConfigurableJoint configurableJoint;
+
+    private Quaternion targetInitialRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        configurableJoint = GetComponent<ConfigurableJoint>();
+        targetInitialRotation = targetLimb.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.rotation = copyRotation();
+    }
+
+    private Quaternion copyRotation(){
+        return Quaternion.Inverse(targetLimb.localRotation) * targetInitialRotation;
+    }
+}
