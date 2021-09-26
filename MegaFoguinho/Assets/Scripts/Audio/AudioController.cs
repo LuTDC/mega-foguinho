dd1fe18d1956003f845f3674c69b8de190cb600b
Paolo <paolo.hitoshi@gmail.com>
2021-09-26 00:07:35 -0300

    Pause e Audio
    Pause feito com botão de resume, ir para o menu e sair do jogo
    Audio feito para criar assets de som e gerenciar sons a partir de qualquer script

---
+++ MegaFoguinho/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    public static AudioController instance;
+
+    void Awake()
+    {
+       CheckInstance();
+
+       AddSource();  
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    void CheckInstance()
+    {
+        if(instance == null)
+        {
+            instance = this; 
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void AddSource()
+    {
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound playSound = Array.Find(sounds, sound => sound.name == name);
+
+        if(playSound == null)
+        {
+            Debug.Log("Sound not found");
+
+            return;
+        }
+
+        playSound.source.Play();
+    }
+}
---
+++ MegaFoguinho/Assets/Scripts/PauseMenu/PauseMenuController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public string loadLevel;
+
+    static bool IsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void LoadMenu(string loadLevel)
+    {
+        Time.timeScale = 1f;
+        IsPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
